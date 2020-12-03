689208abe1ab737c32db0c6056c1586c9f65aa23
8483 <iveselinovski@gmail.com>
2020-12-03 08:37:09 +0100

    refactor day 3, needless overengineering.

--- 2020/3.js
+++ 2020/3.js
@@ -1,4 +1,4 @@
-// 20 min
+// 2 hours
 let fs = require("fs");
 
 let inputFile = `3.txt`;
@@ -19,53 +19,30 @@ let data = input.split(/\r?\n/);
 //     ".#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#",
 // ];
 
-let block = data.map((item) => {
+let slope = data.map((item) => {
     return item.split("");
 });
 
-let slope = [[...block]];
-
-/*
-slope
-    block
-        row = real data 31 chars, 32 indices, test data = 66 chars, 67 indices
-    block
-        row
-*/
-
 function slide(right, down) {
-    let jump = block[0].length;
-    let blockIndex = 0;
+    let jump = slope[0].length;
     let rowIndex = 0;
     let columnIndex = 0;
     let treeCount = 0;
-    // console.log(rowIndex, columnIndex, data[rowIndex]);
-    while (rowIndex < block.length - 1) {
-        //block.length - 1
-        let futureChar = slope[blockIndex][rowIndex + down][columnIndex + right];
-        // console.log("before", blockIndex, rowIndex, columnIndex, treeCount);
-        // console.log(data[rowIndex]);
-        // console.log("future char", futureChar);
+
+    while (rowIndex < slope.length - 1) {
+        let futureChar = slope[rowIndex + down][columnIndex + right];
         if (!futureChar) {
-            slope.push(block);
-            blockIndex++;
             columnIndex -= jump;
         }
 
         rowIndex += down;
         columnIndex += right;
-        let char = slope[blockIndex][rowIndex][columnIndex];
-
-        // console.log("after", blockIndex, rowIndex, columnIndex, treeCount);
-        // console.log(data[rowIndex]);
-        // console.log("current char", char);
+        let char = slope[rowIndex][columnIndex];
 
         if (char === "#") {
             treeCount++;
         }
-        // console.log("\n");
     }
-    // console.log(treeCount);
     return treeCount;
 }
 
@@ -77,8 +54,3 @@ let d = slide(7, 1);
 let e = slide(1, 2);
 
 console.log("part two", a * b * c * d * e);
-
-// ...#.....#..#..#...#.#....##...
-//                               ^ index 30, char 31
-// ...#.....#..#..#...#.#....##...
-//   ^ index 33 (2), char (3)
