ed7ebe196b92477b3dd0869d37a6511e9603a655
8483 <iveselinovski@gmail.com>
2021-12-01 15:36:47 +0100

    Start 2021. Add day one.

---
+++ 2021/1.js
@@ -0,0 +1,30 @@
+// 10 min
+let fs = require("fs");
+
+let inputFile = `1.txt`;
+let input = fs.readFileSync(inputFile, "utf8");
+let data = input.split(/\r?\n/);
+
+data = data.map((item) => parseInt(item));
+
+let count = 0;
+let windowCount = 0;
+
+for (let i = 0; i < data.length; i++) {
+    let previous = data[i - 1];
+    let current = data[i];
+
+    let windowOne = current + data[i + 1] + data[i + 2];
+    let windowTwo = data[i + 1] + data[i + 2] + data[i + 3];
+
+    if (current > previous) {
+        count++;
+    }
+
+    if (windowTwo > windowOne) {
+        windowCount++;
+    }
+}
+
+console.log(count);
+console.log(windowCount);
