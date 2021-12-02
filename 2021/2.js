02cfd9d611adc060be6e9711c7900b71decfd5e7
8483 <iveselinovski@gmail.com>
2021-12-02 06:18:14 +0100

    Day 2 refactor

--- 2021/2.js
+++ 2021/2.js
@@ -7,32 +7,28 @@ let data = input.split(/\r?\n/);
 
 // data = ["forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"];
 
-data = data.map((item) => {
-    let parts = item.split(" ");
-    return {
-        direction: parts[0],
-        value: +parts[1],
-    };
-});
-
 let x = 0,
     y = 0,
     depth = 0;
 
 data.forEach((item) => {
-    switch (item.direction) {
+    let parts = item.split(" ");
+    let direction = parts[0];
+    let value = +parts[1];
+
+    switch (direction) {
         case "forward":
-            x += item.value;
-            depth += y * item.value;
+            x += value;
+            depth += y * value;
 
             break;
 
         case "down":
-            y += item.value;
+            y += value;
             break;
 
         case "up":
-            y -= item.value;
+            y -= value;
             break;
 
         default:
